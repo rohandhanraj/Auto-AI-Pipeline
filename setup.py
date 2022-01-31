d5f39715c802db45afae0d5978d228bf0bcd2f0a
rohandhanraj <rdy5674@gmail.com>
2022-01-31 13:15:30 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Auto AI Pipeline",
+    version="0.0.1",
+    description="Auto ML Application",
+    author="Rohan Dhanraj Yadav", 
+    packages=find_packages(),
+    license="MIT"
+)
